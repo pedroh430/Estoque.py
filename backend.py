0e72ed7dfb494baa3cd6f8156ccadc6072693aca
Pedro Ribeiro <pedrobldap41@gmail.com>
2025-03-24 22:49:48 -0300

    primeiro git

---
+++ backend.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+class Interface:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Estoque de Itens')
+        self.root.geometry('500x400')
+        
+        #label
+        tk.Label()
+        self.tk.Entry()
+        
+        
+        self.root.mainloop()
+        
+     
+    def yyiy(self):
+  
+  
+  
+  
+  
+  
+       
+Interface()
